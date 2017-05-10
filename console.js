1e92950a40221cc9ff184808c4b662436a6cfa52
Ahmet Ertem <amadertem@gmail.com>
2017-05-10 08:42:50 +0300

    fc

--- console.js
+++ console.js
@@ -1,46 +1,78 @@
 #!/usr/bin/env node
 
-'use strict';
+//
+// constants
+//
 const markets_folder = './lib/markets/';
+//
+// libraries
+//
+const _ = require('lodash');
+const market = require('./lib/market.js');
+const pair = require('./lib/pair.js');
 const readline = require('readline');
+const glob = require('glob');
 const fs = require('fs');
-var files = fs.readdirSync(markets_folder);
-files.forEach(file =>
+let files = fs.readdirSync(markets_folder);
+let markets = [];
+glob(markets_folder + "*.js", function(err, files)
 {
-	require(markets_folder + file);
-	console.log(markets_folder + file + ' loaded');
+    files.forEach(file =>
+    {
+        var market_ = require(file);
+        console.log(market_.prototype._pairs);
+        console.log(file + ' loaded');
+        glob(file.replace('.js', '') + "/*.json", function(err_, pairs)
+        {
+            pairs.forEach(pair =>
+            {
+                var o = JSON.parse(fs.readFileSync(pair, 'utf8'));
+                if (_.isArray(o))
+                {
+                    market_._pairs = _.concat(market_.pairs, o);
+                }
+                else
+                {
+                    market_._pairs.push(o);
+                }
+                console.log(pair + ' loaded');
+            });
+        });
+        markets.push(market_);
+    });
 });
+console.log(markets);
 readline.emitKeypressEvents(process.stdin);
 if (process.stdin.isTTY)
 {
-	process.stdin.setRawMode(true);
+    process.stdin.setRawMode(true);
 }
 const rl = readline.createInterface(
 {
-	input: process.stdin,
-	output: process.stdout,
-	prompt: '> '
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> '
 });
 rl.prompt();
 rl.on('line', (line) =>
-	{
-		switch (line.trim())
-		{
-			case 'hello':
-				console.log('world!');
-				break;
-			case 'quit':
-				console.log('Have a great day!');
-				process.exit(0);
-				break;
-			default:
-				console.log(`Say what? I might have heard '${line.trim()}'`);
-				break;
-		}
-		rl.prompt();
-	})
-	.on('close', () =>
-	{
-		console.log('Have a great day!');
-		process.exit(0);
-	});
+    {
+        switch (line.trim())
+        {
+            case 'hello':
+                console.log('world!');
+                break;
+            case 'quit':
+                console.log('Have a great day!');
+                process.exit(0);
+                break;
+            default:
+                console.log(`Say what? I might have heard '${line.trim()}'`);
+                break;
+        }
+        rl.prompt();
+    })
+    .on('close', () =>
+    {
+        console.log('Have a great day!');
+        process.exit(0);
+    });
