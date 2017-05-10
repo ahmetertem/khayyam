c53fcccc500c69019f1f0ed3ba7bb9d3d249b906
Ahmet Ertem <Ahmet Ertem>
2017-05-10 22:18:56 +0300

    base

--- console.js
+++ console.js
@@ -1,78 +1,80 @@
 #!/usr/bin/env node
 
+const app = require('./lib/app.js');
+app.init();
 //
-// constants
-//
-const markets_folder = './lib/markets/';
-//
-// libraries
+// C
+// O
+// N
+// S
+// O
+// L
+// E
 //
 const _ = require('lodash');
-const market = require('./lib/market.js');
-const pair = require('./lib/pair.js');
 const readline = require('readline');
-const glob = require('glob');
-const fs = require('fs');
-let files = fs.readdirSync(markets_folder);
-let markets = [];
-glob(markets_folder + "*.js", function(err, files)
-{
-    files.forEach(file =>
-    {
-        var market_ = require(file);
-        console.log(market_.prototype._pairs);
-        console.log(file + ' loaded');
-        glob(file.replace('.js', '') + "/*.json", function(err_, pairs)
-        {
-            pairs.forEach(pair =>
-            {
-                var o = JSON.parse(fs.readFileSync(pair, 'utf8'));
-                if (_.isArray(o))
-                {
-                    market_._pairs = _.concat(market_.pairs, o);
-                }
-                else
-                {
-                    market_._pairs.push(o);
-                }
-                console.log(pair + ' loaded');
-            });
-        });
-        markets.push(market_);
-    });
-});
-console.log(markets);
 readline.emitKeypressEvents(process.stdin);
 if (process.stdin.isTTY)
 {
-    process.stdin.setRawMode(true);
+	process.stdin.setRawMode(true);
 }
 const rl = readline.createInterface(
 {
-    input: process.stdin,
-    output: process.stdout,
-    prompt: '> '
+	input: process.stdin,
+	output: process.stdout,
+	prompt: '> '
 });
 rl.prompt();
 rl.on('line', (line) =>
-    {
-        switch (line.trim())
-        {
-            case 'hello':
-                console.log('world!');
-                break;
-            case 'quit':
-                console.log('Have a great day!');
-                process.exit(0);
-                break;
-            default:
-                console.log(`Say what? I might have heard '${line.trim()}'`);
-                break;
-        }
-        rl.prompt();
-    })
-    .on('close', () =>
-    {
-        console.log('Have a great day!');
-        process.exit(0);
-    });
+	{
+		line = line.trim();
+		var args_ = line.split(' ');
+		var args_c = args_.length;
+		switch (args_[0])
+		{
+			case 'quit':
+			case 'exit':
+				console.log('Have a great day!');
+				process.exit(0);
+				break;
+			case 'init':
+				if (args_c == 1 || (args_c == 2 && args_[1] == 'list'))
+				{
+					console.log('Available markets:');
+					for (key in app.markets.available_markets)
+					{
+						console.log(key + ' : ' + app.markets.available_markets[key].name);
+					}
+				}
+				else
+				{
+					if (args_c > 1)
+					{
+						args_[1] = _.parseInt(args_[1]);
+						if (args_[1] > -1 && args_[1] < app.markets.available_markets.length)
+						{
+							if (args_c < 4)
+							{
+								console.error('Usage:');
+								console.log('\tinit <market_inde> <api_key> <api_secret>');
+							}
+							else
+							{
+								app.markets.add(app.markets.available_markets[args_[1]].name, args_[2], args_[3]);
+							}
+						}
+						else
+						{
+							console.error('Index `' + args_[1] + '` is not found. Please enter value between 0 and ' + (app.markets.available_markets.length - 1));
+						}
+					}
+				}
+				break;
+		}
+		rl.prompt();
+	})
+	.on('close', () =>
+	{
+		console.log('Have a great day!');
+		process.exit(0);
+	});
