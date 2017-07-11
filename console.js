575a2b8a3b43fc3df7f99d7c5584c3634613cc32
Ahmet Ertem <amadertem@gmail.com>
2017-07-12 00:41:42 +0300

    fixed #17

--- console.js
+++ console.js
@@ -5,24 +5,23 @@ var vorpal = require('vorpal')()
 var chalk = require('chalk')
 var app = require('./lib/app.js')
 app.prerequisite()
-app.initializePlugins()
 app.markets.on('add_available_market', function (a) {
-  console.log(chalk.blue('Market `' + a + '` added to available markets'))
+  app.log(chalk.blue('Market `' + a + '` added to available markets'))
 })
 app.markets.on('market_removed', function () {
-  console.log(chalk.green('Market removed'))
+  app.log(chalk.green('Market removed'))
 })
 app.markets.on('saved', function () {
-  console.log(chalk.blue('Markets saved'))
+  app.log(chalk.blue('Markets saved'))
 })
 app.markets.on('error', function (message) {
-  console.log(chalk.red(message))
+  app.log(chalk.red(message))
 })
 app.markets.on('market_pair_added', function (market, pair) {
-  console.log(chalk.green('Pair `' + pair.currency2.toUpperCase() + '/' + pair.currency1.toUpperCase() + '` added to ' + market.name))
+  app.log(chalk.green('Pair `' + pair.currency2.toUpperCase() + '/' + pair.currency1.toUpperCase() + '` added to ' + market.name))
 })
 app.markets.on('market_pair_removed', function (market) {
-  console.log(chalk.green('Pair is removed from ' + market.name))
+  app.log(chalk.green('Pair is removed from ' + market.name))
 })
 var files = glob.sync('./lib/console/*.js', {
   cwd: __dirname
