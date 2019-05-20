7af81570da26d3e0504691b54ee5340b846ba26f
Ahmet Ertem <amadertem@gmail.com>
2019-05-20 17:42:30 +0300

    pass

--- console.js
+++ console.js
@@ -2,12 +2,14 @@
 
 var glob = require('glob')
 var vorpal = require('vorpal')()
+var inquirer = require('inquirer')
 var chalk = require('chalk')
+
 var app = require('./lib/app.js')
 app.prerequisite()
-app.markets.on('add_available_market', function (a) {
-  app.log(chalk.blue('Market `' + a + '` added to available markets'))
-})
+// app.markets.on('add_available_market', function (a) {
+//   app.log(chalk.blue('Market `' + a + '` added to available markets'))
+// })
 app.markets.on('market_removed', function () {
   app.log(chalk.green('Market removed'))
 })
@@ -17,9 +19,9 @@ app.markets.on('saved', function () {
 app.markets.on('error', function (message) {
   app.log(chalk.red(message))
 })
-app.markets.on('market_pair_added', function (market, pair) {
-  app.log(chalk.green('Pair `' + pair.currency2.toUpperCase() + '/' + pair.currency1.toUpperCase() + '` added to ' + market.name))
-})
+// app.markets.on('market_pair_added', function (market, pair) {
+//   app.log(chalk.green('Pair `' + pair.currency2.toUpperCase() + '/' + pair.currency1.toUpperCase() + '` added to ' + market.name))
+// })
 app.markets.on('market_pair_removed', function (market) {
   app.log(chalk.green('Pair is removed from ' + market.name))
 })
@@ -29,17 +31,25 @@ var files = glob.sync('./lib/console/*.js', {
 files.forEach(file => {
   vorpal.use(require(file), app)
 })
-app.init()
-vorpal.find('exit')
-  .remove()
-vorpal.command('exit', 'Exits application.')
-  .alias('quit')
-  .action(function () {
-    process.exit(0)
-  })
-vorpal.history('bitcoinbot')
-  .delimiter('$')
-  .show()
+inquirer.prompt([{
+    type: 'password',
+    name: 'pass',
+    message: 'Enter your password if you have, otherwise just press enter to continue...'
+}])
+  .then(function (answers) {
+    app.init(answers.pass)
+    vorpal.find('exit')
+      .remove()
+    vorpal.command('exit', 'Exits application.')
+      .alias('quit')
+      .action(function () {
+        process.exit(0)
+      })
+    vorpal.history('bitcoinbot')
+      .delimiter('$')
+      .show()
+  });
+// console.log(process.argv)
 
 /*eslint no-unused-vars: ["error", { "vars": "local" }]*/
 /*global clean:true */
